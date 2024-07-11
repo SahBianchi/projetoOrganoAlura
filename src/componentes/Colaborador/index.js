99476b797501242965d008cdbd20a1cf3fa9bf67
SahBianchi <samantha.sbianchi@gmail.com>
2024-07-11 15:49:17 -0300

    fix: Projeto Organo 2 - Alterando componentes

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,14 @@
+import { AiFillCloseCircle } from 'react-icons/ai'
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+    return (
+    <div className="colaborador">
+        <AiFillCloseCircle
+            size={25}
+            className='deletar'
+            onClick={() => aoDeletar(colaborador.id)}
+        />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
